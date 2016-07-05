9fbc3da18376b8b5800e9c876e4dd4305dded934
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-05 11:52:23 -0400

    First pass at module structure that includes test coverage files

---
+++ test/quicksiler-config-client.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * QuicksilverConfig.test
+ */
+require('dotenv').config({ silent: true });
+const QuicksilverConfigClient = require('../lib/quicksilver-config-client');
+
+/**
+ * Test Quicksilver Nodejs config.
+ */
+describe('QuicksilverConfigClient', () => {
+
+
+});
