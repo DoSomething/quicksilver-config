ee00ee6ece1e21daeba8fc0fb8b31853a3e89243
Darren Dee Lee <darrendouglas.lee@gmail.com>
2016-07-05 13:02:11 -0400

    Stubs of basic functionality

--- test/quicksiler-config-client.test.js
+++ test/quicksiler-config-client.test.js
@@ -10,6 +10,12 @@ const QuicksilverConfigClient = require('../lib/quicksilver-config-client');
  * Test Quicksilver Nodejs config.
  */
 describe('QuicksilverConfigClient', () => {
-
-
+  /**
+   * Helper: get default config client.
+   */
+  function getConfigClient() {
+    return new QuicksilverConfigClient({
+      mbConfigPath: process.env.MB_CONFIG_PATH,
+    });
+  }
 });
