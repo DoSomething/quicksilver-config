e29cc0f845fbbc35dfd1831f637566bc57d79537
Sergii Tkachenko <hi@sergii.org>
2016-07-13 22:32:09 +0300

    Use config.json included into the repo.

--- test/quicksiler-config-client.test.js
+++ test/quicksiler-config-client.test.js
@@ -16,33 +16,21 @@ describe('QuicksilverConfigClient', () => {
    * Helper: get default config client.
    */
   function getConfigClient() {
-    return new QuicksilverConfigClient({
-      qsConfigPath: process.env.QS_CONFIG_PATH,
-    });
+    return new QuicksilverConfigClient();
   }
 
 
   // Constructor.
   describe('constructor', () => {
     // Test new instance.
-    it('without options should throw a TypeError', () => {
-      (() => new QuicksilverConfigClient()).should.throw(TypeError);
-    });
-
-    // Check QS_CONFIG_PATH has a value.
-    it('QS_CONFIG_PATH in process.env should be set', () => {
-      process.env.QS_CONFIG_PATH.should.be.not.empty();
-    });
-
-    // Test new instance.
     it('should create new instance configured correctly', () => {
       const client = getConfigClient();
       should(client).be.an.instanceOf(QuicksilverConfigClient);
-      client.should.have.property('qsConfigPath').which.is.not.empty();
+      client.should.have.property('qsConfigPath').which.is.equal('config.json');
     });
 
     // Check QS_CONFIG_PATH has valid value by confirming access to the path to the JSON file.
-    it('QS_CONFIG_PATH in process.env should point to valid JSON file', () => {
+    it('config file should be exist', () => {
       const client = getConfigClient();
       const configFileFound = client.configFileFound();
       return configFileFound.should.eventually.equal(true);
@@ -50,14 +38,14 @@ describe('QuicksilverConfigClient', () => {
   });
 
   // getAllSettings().
-  describe('getAllSettings', () => {
+  describe('getAllSettings()', () => {
     // Check getSettings method.
-    it('getAllSettings() should be exposed', () => {
+    it('should be exposed', () => {
       getConfigClient().getAllSettings.should.be.a.Function();
     });
 
     // Get all settings.
-    it('should return all settings in mb_config.json', () => {
+    it('should return all settings in the config file', () => {
       // Check response to be an object.
       const client = getConfigClient();
       should(client).be.an.instanceOf(QuicksilverConfigClient);
